366db65825114cb71d5b025988dbe4e8d8dc0e5c
kUSHAL <majmundarkushal9@gmail.com>
2019-07-07 21:16:20 +0530

    Feature Vectoe Based Updated

--- BridgeCode/script_feature_vector.py
+++ BridgeCode/script_feature_vector.py
@@ -15,7 +15,7 @@ def read_file(filename):
 		trader.append(i[0])
 		timestamp.append(i[1])
 		features.append(i[2:-1])
-		severity.append(i[-1])
+		severity.append(float(i[-1]))
 	return (trader,timestamp,features,severity)
 
 def do_cluster(features,no_traders):
@@ -23,35 +23,43 @@ def do_cluster(features,no_traders):
 	kmeans=KMeans(n_clusters=no_traders,random_state=0).fit(features)
 	return list(kmeans.labels_)
 
-def main():
+def main(no_analysts,threshold_k):
 	(trader,timestamp,features,severity)=read_file('feature_vector.csv')
 	traders=list(set(trader))
 	traders.sort()
-	labels=do_cluster(features,len(set(trader)))
-	match={}
-	for i in range(len(trader)):
-		for j in range(len(traders)):
-			match[trader[i]+','+str(j)]=0
-	for i in range(len(trader)):
-		match[trader[i]+','+str(labels[i])]+=1
-	a1=list(match.keys())
-	a1.sort(key=lambda i:match[i],reverse=True)
-	#print(a1)
-	map_cluster_trader={}
-	vis=[]
-	for i in a1:
-		x,y=i.split(',')
-		y=int(y)
-		if y not in map_cluster_trader and x not in vis:
-			map_cluster_trader[y]=x
-			vis.append(x)
+	labels=do_cluster(features,no_analysts)
+#	match={}
+#	for i in range(len(trader)):
+#		for j in range(len(traders)):
+#			match[trader[i]+','+str(j)]=0
+#	for i in range(len(trader)):
+#		match[trader[i]+','+str(labels[i])]+=1
+#	a1=list(match.keys())
+#	a1.sort(key=lambda i:match[i],reverse=True)
+#	#print(a1)
+#	map_cluster_trader={}
+#	vis=[]
+#	for i in a1:
+#		x,y=i.split(',')
+#		y=int(y)
+#		if y not in map_cluster_trader and x not in vis:
+#			map_cluster_trader[y]=x
+#			vis.append(x)
 #	print(map_cluster_trader)
-	allocation={}
-	for i in traders:
-		allocation[i]=[]
-	for i in range(len(labels)):
-		allocation[map_cluster_trader[labels[i]]].append(i)
+#	allocation={}
+#	for i in traders:
+#		allocation[i]=[]
+#	for i in range(len(labels)):
+#		allocation[map_cluster_trader[labels[i]]].append(i)
 #	print(allocation)
-	return allocation
-
-print(main())
+#	return allocation
+	map_analyst_labels={}
+	for i in range(no_analysts):
+		map_analyst_labels[i]=[]
+	for i in range(len(labels)):
+		map_analyst_labels[labels[i]].append(i)
+	for i in map_analyst_labels:
+		map_analyst_labels[i].sort(key=lambda i:severity[i],reverse=True)
+		map_analyst_labels[i]=map_analyst_labels[i][:threshold_k]
+	return map_analyst_labels
+print(main(3,5))
