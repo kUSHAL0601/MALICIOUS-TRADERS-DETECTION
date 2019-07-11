6b5dd92be8ccb1e9ff3063e320b87cd76a57222d
kUSHAL <majmundarkushal9@gmail.com>
2019-07-11 10:27:34 +0530

    Labels of ground truth added

--- BridgeCode/script_feature_vector.py
+++ BridgeCode/script_feature_vector.py
@@ -7,16 +7,19 @@ def read_file(filename):
 		for i in l:
 			lines.append(i.strip().split(','))
 	headings=lines.pop(0)
+	#print(headings)
 	trader=[]
 	timestamp=[]
 	features=[]
 	severity=[]
+	labels=[]
 	for i in lines:
 		trader.append(i[0])
 		timestamp.append(i[1])
-		features.append(i[2:-1])
-		severity.append(float(i[-1]))
-	return (trader,timestamp,features,severity)
+		features.append(i[2:-2])
+		severity.append(float(i[-2]))
+		labels.append(float(i[-1]))
+	return (trader,timestamp,features,severity,labels)
 
 def do_cluster(features,no_traders,cluster_centers):
 	features=np.array(features)
@@ -28,36 +31,12 @@ def do_cluster(features,no_traders,cluster_centers):
 	return kmeans
 
 def main(no_analysts,threshold_k,cluster_centers=[]):
-	(trader,timestamp,features,severity)=read_file('feature_vector.csv')
+	(trader,timestamp,features,severity,labels_gt)=read_file('features_rbf/feature_vector_0.csv')
+#	print(labels_gt)
 	traders=list(set(trader))
 	traders.sort()
 	kmeans=do_cluster(features,no_analysts,cluster_centers)
 	labels=kmeans.labels_
-#	match={}
-#	for i in range(len(trader)):
-#		for j in range(len(traders)):
-#			match[trader[i]+','+str(j)]=0
-#	for i in range(len(trader)):
-#		match[trader[i]+','+str(labels[i])]+=1
-#	a1=list(match.keys())
-#	a1.sort(key=lambda i:match[i],reverse=True)
-#	#print(a1)
-#	map_cluster_trader={}
-#	vis=[]
-#	for i in a1:
-#		x,y=i.split(',')
-#		y=int(y)
-#		if y not in map_cluster_trader and x not in vis:
-#			map_cluster_trader[y]=x
-#			vis.append(x)
-#	print(map_cluster_trader)
-#	allocation={}
-#	for i in traders:
-#		allocation[i]=[]
-#	for i in range(len(labels)):
-#		allocation[map_cluster_trader[labels[i]]].append(i)
-#	print(allocation)
-#	return allocation
 	map_analyst_labels={}
 	for i in range(no_analysts):
 		map_analyst_labels[i]=[]
@@ -69,12 +48,12 @@ def main(no_analysts,threshold_k,cluster_centers=[]):
 	for i in map_analyst_labels:
 		x=[]
 		for j in map_analyst_labels[i]:
-			x.append(trader[j])
+			x.append((trader[j],labels_gt[j]))
 		map_analyst_labels[i]=x
 	return map_analyst_labels,kmeans.cluster_centers_
 allocation,cluster_center=main(3,5)
 print(allocation)
-print(cluster_center)
+#print(cluster_center)
 allocation,cluster_center=main(3,5,cluster_center)
 print(allocation)
-print(cluster_center)
+#print(cluster_center)
