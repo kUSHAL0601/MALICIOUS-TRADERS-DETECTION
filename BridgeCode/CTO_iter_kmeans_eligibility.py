b9eca93d2e3f6cac853bfe5225d2eaf581dd3f13
kUSHAL <majmundarkushal9@gmail.com>
2019-07-13 18:22:34 +0530

    Eligibility score based

--- BridgeCode/CTO_iter_kmeans_eligibility.py
+++ BridgeCode/CTO_iter_kmeans_eligibility.py
@@ -3,22 +3,26 @@ from sklearn.cluster import KMeans
 import os,os.path
 
 eligibility={}
+frequency={}
+## Function to get top tuple not taken for a cluster
+def get_top(arr_cluster,vis,trader,severity):
+	max_eligibility=0
+	topTuple=0
+	for i in arr_cluster:
+		if i not in vis:
+			new_val=(eligibility[trader[i]]+frequency[trader[i]]+1)*severity[i]
+			if new_val>max_eligibility:
+				max_eligibility=new_val
+				topTuple=i
+	eligibility[trader[topTuple]]=0.25*max_eligibility
+	for i in set(trader):
+		if i != trader[topTuple]:
+			eligibility[i]*=0.9
+	return topTuple
 
-def update_eligibility(trader,severity,discount_f):
-	traders=list(set(trader))
-	sum_t={}
-	for i in traders:
-		sum_t[i]=0
-		if i not in eligibility:
-			eligibility[i]=0
-	norm_severity={}
-	for i in range(len(trader)):
-		sum_t[trader[i]]+=severity[i]
-		norm_severity[i]=severity[i]
-	for i in range(len(trader)):
-		norm_severity[i]/=sum_t[trader[i]]
-	return norm_severity
-	
+def update_frequency(trader):
+	for i in trader:
+		frequency[i]+=1
 
 def read_file(filename):
 	with open(filename,'r') as f:
@@ -54,8 +58,11 @@ def cto(no_analysts,threshold_k,i,cluster_centers=[]):
 	(trader,timestamp,features,severity,labels_gt)=read_file('features_rbf/feature_vector_'+str(i)+'.csv')
 #	print(labels_gt)
 	traders=list(set(trader))
+	for i in traders:
+		if i not in eligibility:
+			eligibility[i]=0.0
+			frequency[i]=0
 	traders.sort()
-	#norm_severity=update_eligibility(trader,severity,)
 	kmeans=do_cluster(features,no_analysts,cluster_centers)
 	labels=kmeans.labels_
 	map_analyst_labels={}
@@ -67,14 +74,25 @@ def cto(no_analysts,threshold_k,i,cluster_centers=[]):
 	for i in range(no_analysts):
 		cluster_size.append(len(map_analyst_labels[i]))
 	print(cluster_size)
+	inc_trades=[]
 	for i in map_analyst_labels:
 		map_analyst_labels[i].sort(key=lambda i:severity[i],reverse=False)
-		map_analyst_labels[i]=map_analyst_labels[i][:threshold_k]
+		top_labels=[]
+		vis=set()
+		for _ in range(threshold_k):
+			x=get_top(map_analyst_labels[i],vis,trader,severity)
+			vis.add(x)
+			top_labels.append(x)
+		map_analyst_labels[i]=top_labels
+		inc_trades+=top_labels
+		#map_analyst_labels[i]=map_analyst_labels[i][:threshold_k]
 	for i in map_analyst_labels:
 		x=[]
 		for j in map_analyst_labels[i]:
 			x.append((trader[j],labels_gt[j]))
 		map_analyst_labels[i]=x
+
+	update_frequency(trader)
 	return map_analyst_labels,kmeans.cluster_centers_,cluster_size
 
 def get_positives_negatives(allocation):
