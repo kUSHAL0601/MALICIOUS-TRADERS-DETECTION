89cb5b0a0d2381702c675d803ddbafdf71ef2afe
kUSHAL <majmundarkushal9@gmail.com>
2019-09-01 23:37:59 +0530

    Updated

--- BridgeCode/mod/cto_eligibility_v1.py
+++ BridgeCode/mod/cto_eligibility_v1.py
@@ -6,30 +6,32 @@ eligibility={}
 frequency={}
 rank={}
 
-def get_top(arr_cluster,vis,trader,severity):
+def get_top(arr_cluster,vis,trader,severity,idx):
 	max_eligibility=1e9
 	topTuple=0
 	for i in arr_cluster:
 		if i not in vis:
-			new_val=(eligibility[trader[i]]+frequency[trader[i]]+1)*severity[i]
+			new_val=(eligibility[idx][trader[i]]+frequency[idx][trader[i]]+1)*severity[i]
 			if new_val<max_eligibility:
 				max_eligibility=new_val
 				topTuple=i
-	eligibility[trader[topTuple]]=0.25*max_eligibility
+	eligibility[idx][trader[topTuple]]=0.25*max_eligibility
 	for i in set(trader):
 		if i != trader[topTuple]:
-			eligibility[i]*=0.9
-	# frequency[trader[topTuple]]+=1
+			eligibility[idx][i]*=0.9
 	return topTuple
 
-def update_frequency(trader):
-	# return
-	for i in trader:
-		frequency[i]+=1
+def update_frequency(labels_idx,trader,idx):
+	for i in labels_idx:
+		frequency[idx][trader[i]]+=1
 
 #_non_normalized
 # _non_norm_min
 def cto(no_analysts,threshold_k,index,option_distance,option_clustering,severity_threshold,file_dir,cluster_centers=[]):
+	for i in range(no_analysts):
+		if i not in eligibility:
+			eligibility[i]={}
+			frequency[i]={}
 	(trader,timestamp,features,severity,labels_gt)=read_file(file_dir+'/feature_vector_'+str(index)+'.csv')
 	features=update_features(features,option_distance)
 	labels_gt_arr = np.asarray(labels_gt)
@@ -38,10 +40,11 @@ def cto(no_analysts,threshold_k,index,option_distance,option_clustering,severity
 	pos_labels_gt = labels_gt.count(1)
 	neg_labels_gt = labels_gt.count(-1)
 	traders=list(set(trader))
-	for i in traders:
-		if i not in eligibility:
-			eligibility[i]=0.0
-			frequency[i]=0
+	for j in range(no_analysts):
+		for i in traders:
+			if i not in eligibility[j]:
+				eligibility[j][i]=0.0
+				frequency[j][i]=0
 	traders.sort()
 	kmeans=do_cluster(features,no_analysts,cluster_centers,severity,trader,option_clustering)
 	labels=kmeans.labels_
@@ -60,10 +63,11 @@ def cto(no_analysts,threshold_k,index,option_distance,option_clustering,severity
 		top_labels=[]
 		vis=set()
 		for _ in range(threshold_k):
-			x=get_top(map_analyst_labels[i],vis,trader,severity)
+			x=get_top(map_analyst_labels[i],vis,trader,severity,i)
 			print(trader[x],end=' ')
 			vis.add(x)
 			top_labels.append(x)
+		update_frequency(map_analyst_labels[i],trader,i)
 		map_analyst_labels[i]=top_labels
 		inc_trades+=top_labels
 		#map_analyst_labels[i]=map_analyst_labels[i][:threshold_k]
@@ -76,12 +80,12 @@ def cto(no_analysts,threshold_k,index,option_distance,option_clustering,severity
 
 	# print(eligibility)
 	eligibility_arr=sorted([[eligibility[i],frequency[i],i] for i in eligibility])
-	for i in range(len(eligibility_arr)):
-		try:
-			rank[eligibility_arr[i][2]]+=i
-		except:
-			rank[eligibility_arr[i][2]]=i
+	for j in eligibility_arr:
+		for i in range(len(eligibility_arr[j])):
+			try:
+				rank[eligibility_arr[j][i][2]]+=i
+			except:
+				rank[eligibility_arr[j][i][2]]=i
 
 	print("\n\n\n",sorted(eligibility_arr),sorted(list(rank.keys()),key=lambda i: rank[i]),"\n\n\n")
-	update_frequency(trader)
 	return map_analyst_labels,kmeans.cluster_centers_,cluster_size,neg_labels_gt
