09ad637c1593f8349f95c6382012799f1b7ad673
kUSHAL <majmundarkushal9@gmail.com>
2019-09-01 22:02:36 +0530

    Updated

--- BridgeCode/MODULAR/cto_eligibility.py
+++ BridgeCode/MODULAR/cto_eligibility.py
@@ -5,34 +5,39 @@ from distance import *
 eligibility={}
 frequency={}
 
-def get_top(arr_cluster,vis,trader,severity):
+def get_top(arr_cluster,vis,trader,severity,idx):
 	max_eligibility=1e9
 	topTuple=0
 	for i in arr_cluster:
 		if i not in vis:
-			new_val=(eligibility[trader[i]]+frequency[trader[i]]+1)*severity[i]
+			new_val=(eligibility[idx][trader[i]]+frequency[idx][trader[i]]+1)*severity[i]
 			if new_val<max_eligibility:
 				max_eligibility=new_val
 				topTuple=i
-	eligibility[trader[topTuple]]=0.25*max_eligibility
+	eligibility[idx][trader[topTuple]]=0.25*max_eligibility
 	for i in set(trader):
 		if i != trader[topTuple]:
-			eligibility[i]*=0.9
+			eligibility[idx][i]*=0.9
 	return topTuple
 
-def update_frequency(trader):
-	for i in trader:
-		frequency[i]+=1
+def update_frequency(labels_idx,trader,idx):
+	for i in labels_idx:
+		frequency[idx][trader[i]]+=1
 
 
 def cto(no_analysts,threshold_k,index,option_distance,option_clustering,cluster_centers=[]):
+	for i in range(no_analysts):
+		if i not in eligibility:
+			eligibility[i]={}
+			frequency[i]={}
 	(trader,timestamp,features,severity,labels_gt)=read_file('features_rbf/feature_vector_'+str(index)+'.csv')
 	features=update_features(features,option_distance)
 	traders=list(set(trader))
-	for i in traders:
-		if i not in eligibility:
-			eligibility[i]=0.0
-			frequency[i]=0
+	for j in range(no_analysts):
+		for i in traders:
+			if i not in eligibility[j]:
+				eligibility[j][i]=0.0
+				frequency[j][i]=0
 	traders.sort()
 	kmeans=do_cluster(features,no_analysts,cluster_centers,severity,trader,option_clustering)
 	labels=kmeans.labels_
@@ -51,9 +56,10 @@ def cto(no_analysts,threshold_k,index,option_distance,option_clustering,cluster_
 		top_labels=[]
 		vis=set()
 		for _ in range(threshold_k):
-			x=get_top(map_analyst_labels[i],vis,trader,severity)
+			x=get_top(map_analyst_labels[i],vis,trader,severity,i)
 			vis.add(x)
 			top_labels.append(x)
+		update_frequency(map_analyst_labels[i],trader,i)
 		map_analyst_labels[i]=top_labels
 		inc_trades+=top_labels
 		#map_analyst_labels[i]=map_analyst_labels[i][:threshold_k]
@@ -63,5 +69,4 @@ def cto(no_analysts,threshold_k,index,option_distance,option_clustering,cluster_
 			x.append((trader[j],labels_gt[j]))
 		map_analyst_labels[i]=x
 
-	update_frequency(trader)
 	return map_analyst_labels,kmeans.cluster_centers_,cluster_size
